773943d8f2151d58241f24891ad1b6daedaf15c2
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-06-23 18:15:59 +0900

    add example.sh

---
+++ job/example.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+#### ZCON Example
+
+set -euo pipefail
+source $(dirname "$0")/profile.sh
+
+## Library
+function import_lib() {
+    source ${LIB}/function.sh
+    source ${LIB}/datasets.sh
+    source ${LIB}/blast.sh
+    source ${LIB}/mcscan.sh
+    source ${LIB}/mummer.sh
+    source ${LIB}/meme.sh
+}
+
+## Job
+function job_19221111() {
+    echo "Happy Birthday!"
+    fetch_genome_by_genus "Homo"
+}
+
+## Run
+function main() {
+    import_lib
+    job_19221111
+}
+
+main
