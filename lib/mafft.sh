d508cc90eff988d7b2acf8501c7342fa14d4121e
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-05 14:52:25 +0900

    update mafft.sh

--- lib/mafft.sh
+++ lib/mafft.sh
@@ -3,7 +3,7 @@
 #### Function libs with MAFFT
 
 function construct_msa_pep_blastp_genus_symbol() {
-    ## This function is depend on construct_pep_blastp_genus_symbol, blast.sh
+    ## This function is based on construct_pep_blastp_genus_symbol, blast.sh
     function usage() {
         cat <<EOS
 Usage:  construct_msa_pep_blastp_genus_symbol <arg1> <arg2> <arg3> (<arg4> <arg5> ...)
@@ -31,5 +31,39 @@ EOS
     main "$@"
 }
 
+function addn_contruct_msa_pep_blastp_genus_symbol() {
+    ## This function is based on addn_construct_pep_blastp_genus_symbol, blast.sh
+    function usage() {
+        cat <<EOS
+Usage:  addn_contruct_msa_pep_blastp_genus_symbol <arg1> <arg2> (<arg3> <arg4> <arg5> ...) <arg1> <arg2> <arg3> (<arg4> <arg5> ...)
+
+    arg1: cons_genus
+    arg2: num [number of add_genus]
+    arg3: add1_genus
+    arg4: add2_genus
+    arg5: add3_genus
+    ...
+    arg1: symbol
+    arg2: symbol_org
+    arg3: evalue
+    arg4: pid
+    arg5: pnm
+    ...
+
+EOS
+        exit 1
+    }
+
+    function construct_msa() {
+        mafft "${taskdir}/${genus}.${symbol}.pep.fasta" > "${taskdir}/${genus}.${symbol}.pep.aln"
+    } 
+
+    function main() {
+        addn_construct_pep_blastp_genus_symbol
+        construct_msa
+    }
+
+    main "$@" 
+}
 
 
