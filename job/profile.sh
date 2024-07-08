e7276b617a814b3446e2f4a3a08d95fb2eb93c54
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-08 15:25:40 +0900

    update profile.sh

--- job/profile.sh
+++ job/profile.sh
@@ -1,34 +1,23 @@
 #!/usr/bin/env zsh
 
-setopt errexit
-setopt nounset
-setopt pipefail
-
 #### ZCON Profile
 
-function declare_dirs() {
-    export ROOT=${HOME}/zcon
-    export DATA=${ROOT}/data
-    export DOC=${ROOT}/doc
-    export DOWNLOAD=${ROOT}/download
-    export JOB=${ROOT}/job
-    export LIB=${ROOT}/lib
-    export SCRIPT=${ROOT}/script
-    export TASKFILE=${ROOT}/taskfile
-}
+setopt err_exit
+setopt no_unset
+setopt pipe_fail
 
-function make_dirs() {
-    local _dirs; _dirs=($DATA $DOC $DOWNLOAD $JOB $LIB $SCRIPT $TASKFILE)
-    for _dir in ${_dirs[*]}; do
-        mkdir -p "$_dir"
-    done
-}
+export ROOT=${HOME}/zcon
+export DATA=${ROOT}/data
+export DOC=${ROOT}/doc
+export DOWNLOAD=${ROOT}/download
+export JOB=${ROOT}/job
+export LIB=${ROOT}/lib
+export SCRIPT=${ROOT}/script
+export TASKFILE=${ROOT}/taskfile
 
-function main() {
-    declare_dirs
-    make_dirs
-}
-
-main
+local _dirs; _dirs=($DATA $DOC $DOWNLOAD $JOB $LIB $SCRIPT $TASKFILE)
+for _dir in ${_dirs[*]}; do
+    mkdir -p "$_dir"
+done
 
 alias pyvenv='source ${ROOT}/venv/bin/activate && ${ROOT}/venv/bin/python3'
