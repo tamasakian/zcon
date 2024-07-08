3414e060fdc888955d0018e5f25304600e1ccd0e
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-08 14:58:24 +0900

    update profile.sh

--- job/profile.sh
+++ job/profile.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env zsh
 
+setopt errexit
+setopt nounset
+setopt pipefail
+
 #### ZCON Profile
 
-function declare_dir() {
-    readonly ROOT=${HOME}/zcon
-    readonly DATA=${ROOT}/data
-    readonly DOC=${ROOT}/doc
-    readonly DOWNLOAD=${ROOT}/download
-    readonly JOB=${ROOT}/job
-    readonly LIB=${ROOT}/lib
-    readonly SCRIPT=${ROOT}/script
-    readonly TASKFILE=${ROOT}/taskfile
+function declare_dirs() {
+    export ROOT=${HOME}/zcon
+    export DATA=${ROOT}/data
+    export DOC=${ROOT}/doc
+    export DOWNLOAD=${ROOT}/download
+    export JOB=${ROOT}/job
+    export LIB=${ROOT}/lib
+    export SCRIPT=${ROOT}/script
+    export TASKFILE=${ROOT}/taskfile
 }
 
-function make_dir() {
+function make_dirs() {
     local _dirs; _dirs=($DATA $DOC $DOWNLOAD $JOB $LIB $SCRIPT $TASKFILE)
     for _dir in ${_dirs[*]}; do
         mkdir -p "$_dir"
     done
 }
 
-function declare_interpreter() {
-    readonly PYTHON3=${ROOT}/venv/bin/python3.12
-}
-
 function main() {
-    declare_dir
-    make_dir
-    declare_interpreter
+    declare_dirs
+    make_dirs
 }
 
 main
+
+alias pyvenv='source ${ROOT}/venv/bin/activate && ${ROOT}/venv/bin/python3'
