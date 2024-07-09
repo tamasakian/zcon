e45161c2de2b1c175975e98d57340ab8ed52ccff
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-09 15:06:24 +0900

    update biotp.zsh

--- lib/biotp.zsh
+++ lib/biotp.zsh
@@ -5,7 +5,7 @@
 function construct_all_introns() {
     function usage() {
         cat << EOS
-Usage:  construct_all_introns <arg1>
+Usage:  construct_all_introns <arg1> <arg2>
 
     arg1: genus
     arg2: org
@@ -42,6 +42,56 @@ EOS
     function main() {
         parse_args "$@"
         make_dir
+        generate_introns_gff
+        generate_introns_fasta
+    }
+
+    main "$@"
+}
+
+function construct_all_introns_by_genus() {
+    function usage() {
+        cat << EOS
+Usage:  construct_all_introns_by_genus <arg1>
+
+    arg1: genus
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# != 1 ]]; then
+            usage
+        fi
+        genus="$1"
+    }
+
+    function make_dir() {
+        taskdir=$(make_dir_by_date $TASKFILE)
+    }
+
+    function generate_introns_gff() {
+        for org in ${org_li[*]}; do
+            python3 -m biotp generate_coordinate_all_introns \
+                "${DATA}/${genus}/${org}.genome.gff" \
+                "${taskdir}/${org}.genome.intron.gff"
+        done
+    }
+
+    function generate_introns_fasta() {
+        for org in ${org_li[*]}; do
+            python3 -m biotp generate_all_introns \
+                "${DATA}/${genus}/${org}.dna.toplevel.fasta" \
+                "${taskdir}/${org}.genome.intron.gff" \
+                "${taskdir}/${org}.intron.all.fasta"
+        done
+    }
+
+    function main() {
+        parse_args "$@"
+        make_dir
+        redeclare_genome_by_genus "$genus"
         generate_introns_gff
         generate_introns_fasta
     }
