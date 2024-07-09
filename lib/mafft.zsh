2dd78051b1efc697a8b44327de969f9d3a205509
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-09 10:06:09 +0900

    refactor mafft.zsh

--- lib/mafft.zsh
+++ lib/mafft.zsh
@@ -3,16 +3,19 @@
 #### Function libs with MAFFT
 
 function construct_msa_pep_blastp_genus_symbol() {
-    ## This function is based on construct_pep_blastp_genus_symbol, blast.zsh
+    ## This function is based on construct_pep_blastp_genus_symbol
+    ## which is located in blast.zsh
     function usage() {
         cat <<EOS
-Usage:  construct_msa_pep_blastp_genus_symbol <arg1> <arg2> <arg3> (<arg4> <arg5> ...)
+Usage:  construct_msa_pep_blastp_genus_symbol <arg1> <arg2> <arg3> <arg4> (<arg5> <arg6> ...)
 
     arg1: genus
     arg2: symbol
     arg3: symbol_org
-    arg4: pid
-    arg5: pnm
+    arg4: evalue
+
+    arg5: protein_id
+    arg6: protein_name
     ...
 
 EOS
@@ -31,23 +34,25 @@ EOS
     main "$@"
 }
 
-function addn_contruct_msa_pep_blastp_genus_symbol() {
-    ## This function is based on addn_construct_pep_blastp_genus_symbol, blast.zsh
+function addn_construct_msa_pep_blastp_genus_symbol() {
+    ## This function is based on addn_construct_pep_blastp_genus_symbol
+    ## which is located in blast.zsh
     function usage() {
         cat <<EOS
-Usage:  addn_contruct_msa_pep_blastp_genus_symbol <arg1> <arg2> (<arg3> <arg4> <arg5> ...) <arg1> <arg2> <arg3> (<arg4> <arg5> ...)
+Usage:  addn_construct_msa_pep_blastp_genus_symbol <arg1> <arg2> (<arg3> <arg4> ... <arg(n+2)>) <arg(n+3)> <arg(n+4)> <arg(n+5)> (<arg(n+6)> <arg(n+7)> ...)
 
     arg1: cons_genus
     arg2: num [number of add_genus]
     arg3: add1_genus
     arg4: add2_genus
-    arg5: add3_genus
-    ...
-    arg1: symbol
-    arg2: symbol_org
-    arg3: evalue
-    arg4: pid
-    arg5: pnm
+    arg(n+2): addn_genus
+
+    arg(n+3): symbol
+    arg(n+4): symbol_org
+    arg(n+5): evalue
+
+    arg(n+6): pid
+    arg(n+7): pnm
     ...
 
 EOS
@@ -55,11 +60,11 @@ EOS
     }
 
     function construct_msa() {
-        mafft "${taskdir}/${genus}.${symbol}.pep.fasta" > "${taskdir}/${genus}.${symbol}.pep.aln"
-    } 
+        mafft "${taskdir}/${cons_genus}.${symbol}.pep.fasta" > "${taskdir}/${cons_genus}.${symbol}.pep.aln"
+    }
 
     function main() {
-        addn_construct_pep_blastp_genus_symbol
+        addn_construct_pep_blastp_genus_symbol "$@"
         construct_msa
     }
 
