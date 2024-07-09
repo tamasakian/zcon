fa8e96f4f5ffaf63d7f335df6127d7d553213a41
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-07-09 10:14:16 +0900

    update trimal.zsh

--- lib/trimal.zsh
+++ lib/trimal.zsh
@@ -3,16 +3,19 @@
 #### Function libs with trimAl
 
 function trim_msa_pep_blastp_genus_symbol() {
-    ## This is depend on construct_msa_pep_blastp_genus_symbol, mafft.zsh
+    ## This is depend on construct_msa_pep_blastp_genus_symbol
+    ## which is located in mafft.zsh
     function usage() {
         cat <<EOS
-Usage:  trim_msa_pep_blastp_genus_symbol <arg1> <arg2> <arg3> (<arg4> <arg5> ...)
+Usage:  trim_msa_pep_blastp_genus_symbol <arg1> <arg2> <arg3> <arg4> (<arg5> <arg6> ...)
 
     arg1: genus
     arg2: symbol
     arg3: symbol_org
-    arg4: pid
-    arg5: pnm
+    arg4: evalue
+
+    arg5: pid
+    arg6: pnm
     ...
 
 EOS
@@ -22,12 +25,52 @@ EOS
     function trim_msa() {
         trimal \
             -in "${taskdir}/${genus}.${symbol}.pep.aln" \
-            -out "${taskdir}/${genus}.${symbol}.pep.trim.aln"
+            -out "${taskdir}/${genus}.${symbol}.pep.trim.aln" \
             -automted1
     }
 
     function main() {
         construct_msa_pep_blastp_genus_symbol "$@"
+        trim_msa
+    }
+
+    main "$@"
+}
+
+function addn_trim_msa_pep_blastp_genus_symbol() {
+    ## This function is based on addn_construct_msa_pep_blastp_genus_symbol
+    ## which is located in mafft.zsh
+    function usage() {
+        cat <<EOS
+Usage:  addn_trim_msa_pep_blastp_genus_symbol <arg1> <arg2> (<arg3> <arg4> ... <arg(n+2)>) <arg(n+3)> <arg(n+4)> <arg(n+5)> (<arg(n+6)> <arg(n+7)> ...)
+
+    arg1: cons_genus
+    arg2: num [number of add_genus]
+    arg3: add1_genus
+    arg4: add2_genus
+    arg(n+2): addn_genus
+
+    arg(n+3): symbol
+    arg(n+4): symbol_org
+    arg(n+5): evalue
+
+    arg(n+6): pid
+    arg(n+7): pnm
+    ...
+
+EOS
+        exit 1
+    }
+
+    function trim_msa() {
+        trimal \
+            -in "${taskdir}/${cons_genus}.${symbol}.pep.aln" \
+            -out "${taskdir}/${cons_genus}.${symbol}.pep.trim.aln" \
+            -automted1
+    }
+
+    function main() {
+        addn_construct_msa_pep_blastp_genus_symbol "$@"
         trim_msa
     }
 
