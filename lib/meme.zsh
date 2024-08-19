2c2e85043727a8914ff56b4b35cf0c59954b41f7
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-08-19 14:34:02 +0900

    update meme.zsh

--- lib/meme.zsh
+++ lib/meme.zsh
@@ -24,7 +24,7 @@ function download_meme_databases() {
     main
 }
 
-function fimo_dna_upstream_region_genus_symbol_test() {
+function fimo_dna_upstream_region() {
     ## This function is based on construct_dna_upstream_region_genus_symbol
     ## which is located in blast.zsh
     function usage() {
@@ -74,3 +74,106 @@ EOS
     main "$@"
 }
 
+function fimo_protein_genus_symbol() {
+    ## This function is based on construct_pep_blastp_genus_symbol
+    ## which is located in blast.zsh
+    function usage() {
+        cat <<EOS
+Usage:  fimo_protein_genus_symbol <arg1> <arg2> <arg3> <arg4> <arg5> <arg6> (<arg7> <arg8> ...)
+
+    arg1: motif_filename
+    arg2: qvalue (fimo)
+
+    arg3: genus
+    arg4: symbol
+    arg5: symbol_org
+    arg6: evalue (blastn)
+    arg7: pid
+    arg8: pnm
+    ...
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# -lt 8 ]]; then
+            usage
+        fi
+        motif_filename="$1"
+        qvalue=$2
+    }
+
+    function find_individual_motif_occurrences() {
+        fimo \
+            --o "${taskdir}/fimo_out" \
+            --thresh $qvalue \
+            --qv-thresh \
+            --verbosity 1 \
+            "$motif_filename" \
+            "${taskdir}/${genus}.${symbol}.pep.fasta"
+    }
+    
+    function main() {
+        parse_args "$@"
+        construct_pep_blastp_genus_symbol "${@:3}"
+        find_individual_motif_occurrences
+    }
+
+    main "$@"
+}
+
+function fimo_addn_protein_genus_symbol() {
+    ## This function is based on addn_construct_pep_blastp_genus_symbol
+    ## which is located in blast.zsh
+    function usage() {
+        cat <<EOS
+Usage:  fimo_protein_genus_symbol <arg1> <arg2> <arg3> <arg4> <arg5> <arg6> ...
+
+    arg1: motif_filename
+    arg2: qvalue (fimo)
+
+    arg3: cons_genus
+    arg4: num [number of add_genus]
+    arg5: add1_genus
+    arg6: add2_genus
+    arg(n+4): addn_genus
+
+    arg(n+5): symbol
+    arg(n+6): symbol_org
+    arg(n+7): evalue
+
+    arg(n+8): protein_id
+    arg(n+9): protein_name
+    ...
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# -lt 8 ]]; then
+            usage
+        fi
+        motif_filename="$1"
+        qvalue=$2
+    }
+
+    function find_individual_motif_occurrences() {
+        fimo \
+            --o "${taskdir}/fimo_out" \
+            --thresh $qvalue \
+            --qv-thresh \
+            --verbosity 1 \
+            "$motif_filename" \
+            "${taskdir}/${cons_genus}.${symbol}.pep.fasta"
+    }
+    
+    function main() {
+        parse_args "$@"
+        addn_construct_pep_blastp_genus_symbol "${@:3}"
+        find_individual_motif_occurrences
+    }
+
+    main "$@"
+}
