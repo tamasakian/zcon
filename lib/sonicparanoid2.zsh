cf3c16a40bd00aef571cf42a9fe4b0845cbca753
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-06 18:00:23 +0900

    add sonicparanoid2.zsh

---
+++ lib/sonicparanoid2.zsh
@@ -0,0 +1,62 @@
+#!/usr/bin/env zsh
+
+#### Function libs with SonicParanoid2 (Salvatore et al., 2024)
+
+function infer_orthology() {
+    function usage() {
+        cat <<EOS
+Usage: infer_orthology <arg1> <arg2> ...
+
+    arg1: num
+    arg2: org
+    ...
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# -lt 4 ]]; then
+            usage
+        fi
+        
+        num=$1
+        shift
+        orgs=()
+        for ((i=1; i<=num; i++)); do
+            orgs[i]="${1// /_}"
+            echo "${i}:${orgs[i]}"
+            shift
+        done
+    }
+
+    function move_fasta_to_taskdir() {
+        mkdir "${taskdir}/input" "${taskdir}/output"
+        for org in "${orgs[@]}"; do
+            tmpfile=$(mktemp)
+            genus=${org%%_*}
+            python3 -m fasp exclude_isoforms_by_length \
+                "${DATA}/${genus}/${org}.pep.all.fasta" \
+                "$tmpfile" \
+                "${DATA}/${genus}/${org}.genome.gff" 
+            python3 -m fasp exclude_non_nuclear_proteins \
+                "$tmpfile" \
+                "${taskdir}/input/${org}.fasta"
+        done
+    }
+
+    function sonicparanoid_with_singularity() {
+        module load singularity
+        singularity exec /usr/local/biotools/s/sonicparanoid:2.0.8--py312h1f1cfbb_0 \
+            sonicparanoid -i "${taskdir}/input" -o "${taskdir}/output" -t 16
+    }
+
+    function main() {
+        parse_args "$@"
+        make_taskdir
+        move_fasta_to_taskdir
+        sonicparanoid_with_singularity
+    }
+
+    main "$@"
+}
