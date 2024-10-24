2273dc13974708c5e58e035aba8269b0e770f9da
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-24 19:17:42 +0900

    update hmmer.zsh

--- lib/hmmer.zsh
+++ lib/hmmer.zsh
@@ -24,8 +24,83 @@ EOS
     function search_with_hmmscan() {
         hmmscan \
             -o "${taskdir}/out.txt" \
+            --cpu 5 \
+            -E 1e-2 \
+            "${DATA}/Pfam/Pfam-A.hmm" \
+            "${DATA}/${genus}/${org}.pep.all.fasta"
+    }
+
+    function main() {
+        parse_args "$@"
+        make_taskdir
+        search_with_hmmscan
+    }
+
+    main "$@"
+}
+
+
+function search_domain_per-sequence_hits() {
+    function usage() {
+        cat << EOS
+Usage: search_domain_per-sequence_hits <arg1>
+
+    arg1: org
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# != 1 ]]; then
+            usage
+        fi
+        org="${1// /_}"
+        genus=${org%%_*}
+    }
+
+    function search_with_hmmscan() {
+        hmmscan \
             --tblout "${taskdir}/tblout.txt" \
-            --cpu 4 \
+            --cpu 5 \
+            -E 1e-2 \
+            "${DATA}/Pfam/Pfam-A.hmm" \
+            "${DATA}/${genus}/${org}.pep.all.fasta"
+    }
+
+    function main() {
+        parse_args "$@"
+        make_taskdir
+        search_with_hmmscan
+    }
+
+    main "$@"
+}
+
+
+function search_domain_per-domain_hits() {
+    function usage() {
+        cat << EOS
+Usage: search_domain_per-domain_hits <arg1>
+
+    arg1: org
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# != 1 ]]; then
+            usage
+        fi
+        org="${1// /_}"
+        genus=${org%%_*}
+    }
+
+    function search_with_hmmscan() {
+        hmmscan \
+            --domtblout "${taskdir}/tblout.txt" \
+            --cpu 5 \
             -E 1e-2 \
             "${DATA}/Pfam/Pfam-A.hmm" \
             "${DATA}/${genus}/${org}.pep.all.fasta"
