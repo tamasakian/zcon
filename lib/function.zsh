1447655f5e6d2c575f13de4e3ce7ff964f7c4615
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-30 18:22:25 +0900

    improve doc

--- lib/function.zsh
+++ lib/function.zsh
@@ -1,14 +1,11 @@
 #!/usr/bin/env zsh
+# Last updated: 2024-10-30
 
 #### Function libs
+# make_taskir   <- Creates a task directory with a timestamp.
 
 function make_taskdir() {
-    ## This function makes a task directory.
-    function main() {
-        dirname=$(date +"%Y-%m-%d-%H-%M-%S")
-        taskdir=${TASKFILE}/${dirname}
-        mkdir -p $taskdir
-    }
-
-    main
+    dirname=$(date +"%Y-%m-%d-%H-%M-%S")
+    taskdir=${TASKFILE}/${dirname}
+    mkdir -p $taskdir
 }
