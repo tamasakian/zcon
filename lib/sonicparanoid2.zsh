0f54acdaf900eb464ff6ebcc138b8911f5b978a6
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-30 16:54:17 +0900

    update sonicparanoid2.zsh

--- lib/sonicparanoid2.zsh
+++ lib/sonicparanoid2.zsh
@@ -64,3 +64,61 @@ EOS
 
     main "$@"
 }
+
+
+function infer_ortholog_groups() {
+    function usage() {
+        cat <<EOS
+Usage: infer_ortholog_groups <arg1> <arg2> <arg3>
+
+    arg1: num     - Number of organisms
+    arg2: org     - Names of organisms [e.g., "species1" "species2" ... "speciesN"]
+    arg3: threads - Number of threads to use
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# -lt 5 ]]; then
+            usage
+        fi
+        
+        num=$1; shift
+        orgs=()
+        for ((i=1; i<=num; i++)); do
+            orgs[i]="${1// /_}"; shift
+        done
+        threads=$1; shift
+    }
+
+    function move_fasta_to_taskdir() {
+        mkdir ${taskdir}/input ${taskdir}/output
+        for org in "${orgs[@]}"; do
+            genus=${org%%_*}
+            tmpfile1=$(mktemp)
+            tmpfile2=$(mktemp)
+            python3 -m fasp exclude_isoforms_by_length \
+                ${DATA}/${genus}/${org}.pep.all.fasta \
+                $tmpfile1 \
+                ${DATA}/${genus}/${org}.genome.gff 
+            python3 -m fasp exclude_non_nuclear_proteins \
+                $tmpfile1 \
+                $tmpfile2
+            python3 -m fasp prefix_to_sequence_ids \
+                $tmpfile2 \
+                ${taskdir}/input/${org}.fasta \
+                ${org}
+            rm $tmpfile1 $tmpfile2
+        done
+    }
+
+    function main() {
+        parse_args "$@"
+        make_taskdir
+        move_fasta_to_taskdir
+        sonicparanoid -i ${taskdir}/input -o ${taskdir}/output -t $threads
+    }
+
+    main "$@"
+}
