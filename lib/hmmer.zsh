6b71e898e77bd5636d9d1733f0725da6907667ec
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-30 18:43:40 +0900

    update hmmer.zsh

--- lib/hmmer.zsh
+++ lib/hmmer.zsh
@@ -1,14 +1,21 @@
 #!/usr/bin/env zsh
+# Last updated: 2024-10-30
 
-#### Function libs with HMMER3.4
+#### Function libs with HMMER3
+: << 'FUNCTIONS'
+search_domain:              Search for protein domains using hmmscan.
+search_domain_per_sequence: Search for protein domains using hmmscan and Output domain hits per-sequence.
+search_domain_per_domain:   Search for protein domains using hmmscan and Output domain hits per-domain.
+FUNCTIONS
+
 
 function search_domain() {
     function usage() {
         cat << EOS
 Usage: search_domain <arg1> <arg2>
 
-    arg1: org   <- Organism name [e.g., "Arabidopsis thaliana"]
-    arg2: cpu   <- Number of CPU to use [e.g., 2]
+    arg1: org   <- Organism name.        (e.g., "Arabidopsis thaliana")
+    arg2: cpu   <- Number of CPU to use. (e.g., 2)
 
 EOS
         exit 1
@@ -37,13 +44,13 @@ EOS
 }
 
 
-function search_domain_per-sequence_hits() {
+function search_domain_per_sequence() {
     function usage() {
         cat << EOS
-Usage: search_domain_per-sequence_hits <arg1>
+Usage: search_domain_per_sequence <arg1> <arg2>
 
-    arg1: org   <- Organism name [e.g., "Arabidopsis thaliana"]
-    arg2: cpu   <- Number of CPU to use [e.g., 2]
+    arg1: org   <- Organism name.        (e.g., "Arabidopsis thaliana")
+    arg2: cpu   <- Number of CPU to use. (e.g., 2)
 
 EOS
         exit 1
@@ -73,13 +80,13 @@ EOS
 }
 
 
-function search_domain_per-domain_hits() {
+function search_domain_per_domain() {
     function usage() {
         cat << EOS
-Usage: search_domain_per-domain_hits <arg1>
+Usage: search_domain_per_domain <arg1> <arg2>
 
-    arg1: org   <- Organism name [e.g., "Arabidopsis thaliana"]
-    arg2: cpu   <- Number of CPU to use [e.g., 2]
+    arg1: org   <- Organism name.        (e.g., "Arabidopsis thaliana")
+    arg2: cpu   <- Number of CPU to use. (e.g., 2)
 
 EOS
         exit 1
@@ -98,6 +105,7 @@ EOS
         parse_args "$@"
         make_taskdir
         hmmscan \
+            -o ${taskdir}/out.txt \
             --domtblout ${taskdir}/domtblout.txt \
             --cpu $cpu \
             ${DATA}/Pfam/Pfam-A.hmm \
