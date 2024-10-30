97b8fdf5392cade18dd1ea596125e500f6cc7c9b
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-30 18:17:06 +0900

    update gs2.zsh

--- lib/gs2.zsh
+++ lib/gs2.zsh
@@ -1,17 +1,19 @@
 #!/usr/bin/env zsh
+# Last updated: 2024-10-30
 
 #### Function libs with GS2 (Matshui et al., 2019)
+# estimate_gstree   <- Reconstruct gene trees using the Graph Splitting method.
 
 function estimate_gstree() {
     function usage() {
         cat <<EOS
-Usage: estimate_gstree <arg1> <arg2> <arg3> <arg4> ...
+Usage: estimate_gstree <arg1> <arg2> <arg3> <arg4> <arg5>
 
-    arg1: num_orgs
-    arg2: org
-    arg3: num_peps
-    arg4: pep
-    ...
+    arg1: num_orgs  <- Number of organisms.                 (e.g., 1)
+    arg2: org       <- Names  of organisms.                 (e.g., "Arabidopsis thaliana")
+    arg3: num_peps  <- Number of proteins.                  (e.g., 2)
+    arg4: peps      <- Accession IDs and Names of proteins. (e.g., "NP_001326314.1" "AtFLD" "NP_195315.3" "AtFD")
+    arg5: threads   <- Number of threads                    (e.g., 2)
 
 EOS
         exit 1
@@ -22,39 +24,36 @@ EOS
             usage
         fi
         
-        num_orgs=$1
-        shift
+        num_orgs=$1; shift
         orgs=()
         for ((i=1; i<=num_orgs; i++)); do
-            orgs[i]="${1// /_}"
+            orgs[i]="${1// /_}"; shift
             echo "${i}:${orgs[i]}"
-            shift
         done
-        num_peps=$1
-        shift
+        num_peps=$1; shift
         typeset -g -A peps
         for ((i=1; i<=num_peps; i++)); do
-            key="$1"
-            shift
-            value="$1"
-            shift
+            key="$1"; shift
+            value="$1"; shift
             peps[$key]="$value"
-            echo "${i}:${key}:${peps[$key]}"
+            echo "${i}: ${key}"
+            echo "${i}: ${peps[$key]}"
         done
+        threads=$1
     }
 
     function merge_fasta() {
-        touch "${taskdir}/database.fasta"
+        touch ${taskdir}/database.fasta
         for org in "${orgs[@]}"; do
             genus=${org%%_*}
-            cat "${DATA}/${genus}/${org}.pep.all.fasta" >> "${taskdir}/database.fasta"
+            cat ${DATA}/${genus}/${org}.pep.all.fasta >> ${taskdir}/database.fasta
         done
     }
 
     function generate_fasta() {
-        touch "${taskdir}/pep.fasta"
+        touch ${taskdir}/pep.fasta
         makeblastdb \
-            -in "${taskdir}/database.fasta" \
+            -in ${taskdir}/database.fasta \
             -dbtype prot \
             -hash_index \
             -parse_seqids
@@ -62,21 +61,17 @@ EOS
         for pep_key in ${(@k)peps}; do
             tmpfile=$(mktemp)
             blastdbcmd \
-                -entry "$pep_key" \
-                -db "${taskdir}/database.fasta" \
-                -out "$tmpfile"
+                -entry $pep_key \
+                -db ${taskdir}/database.fasta \
+                -out $tmpfile
             python3 -m fasp rename_header \
-                "$tmpfile" \
-                "$tmpfile" \
-                "${peps[$pep_key]}" \
+                $tmpfile \
+                $tmpfile \
+                ${peps[$pep_key]} \
                 ""
-            cat "$tmpfile" >> "${taskdir}/pep.fasta"
-            rm "$tmpfile"
+            cat $tmpfile >> ${taskdir}/pep.fasta
+            rm $tmpfile
         done
-    }
-
-    function run_gs2() {
-        gs2 -e 100 -t 4 -l "${taskdir}/pep.fasta" > "${taskdir}/pep.nwk"
     }
 
     function main() {
@@ -84,7 +79,11 @@ EOS
         make_taskdir
         merge_fasta
         generate_fasta
-        run_gs2
+        gs2 \
+            -e 100 \
+            -t $threads \
+            -l \
+            ${taskdir}/pep.fasta > ${taskdir}/pep.nwk
     }
 
     main "$@"
