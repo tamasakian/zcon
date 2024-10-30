0592235392751d3c4ad41c06a1095271b47773e8
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-10-30 17:27:30 +0900

    update hgt.zsh

--- lib/hgt.zsh
+++ lib/hgt.zsh
@@ -7,12 +7,12 @@ function detect_crossover_besthits() {
         cat <<EOS
 Usage: detect_crossover_besthits <arg1> <arg2> <arg3> <arg4> <arg5> <arg6> 
 
-    arg1: num_sgp
-    arg2: org_sgp
-    arg3: num_grp
-    arg4: org_grp
-    arg5: num_ogp
-    arg6: org_ogp
+    arg1: num_sgp   <- Number of organisms in subgroup (e.g., 3)
+    arg2: org_sgp   <- Names  of organisms in subgroup (e.g., "Cuscuta australis" "Cuscuta campestris" ... "Cuscuta europaea")
+    arg3: num_grp   <- Number of organisms in group
+    arg4: org_grp   <- Names  of organisms in group    (e.g., "species1" "species2" ... "speciesN")
+    arg5: num_ogp   <- Number of organisms in outgroup
+    arg6: org_ogp   <- Names  of organisms in outgroup (e.g., "species1" "species2" ... "speciesN")
 
 EOS
         exit 1
@@ -23,206 +23,199 @@ EOS
             usage
         fi
 
-        num_sgp=$1
-        shift
+        num_sgp=$1; shift
         org_sgp=()
         for ((i=1; i<=num_sgp; i++)); do
-            org_sgp[i]="${1// /_}"
-            echo "${i}:${org_sgp[i]}"
-            shift
+            org_sgp[i]="${1// /_}"; shift
+            echo "${i}: ${org_sgp[i]}"
         done
 
-        num_grp=$1
-        shift
+        num_grp=$1; shift
         org_grp=()
         for ((j=1; j<=num_grp; j++)); do
-            org_grp[j]="${1// /_}"
-            echo "${j}:${org_grp[j]}"
-            shift
+            org_grp[j]="${1// /_}"; shift
+            echo "${j}: ${org_grp[j]}"
         done
 
-        num_ogp=$1
-        shift
+        num_ogp=$1; shift
         org_ogp=()
         for ((k=1; k<=num_ogp; k++)); do
-            org_ogp[k]="${1// /_}"
-            echo "${k}:${org_ogp[k]}"
-            shift
+            org_ogp[k]="${1// /_}"; shift
+            echo "${k}: ${org_ogp[k]}"
         done
 
-        total=$((num_sgp + num_grp + num_ogp))
-        max_target_seqs=$((total * 10))
-        echo "max_target_seqs:${max_target_seqs}"
+        max_target_seqs=$(((num_sgp + num_grp + num_ogp) * 10))
+        echo "max_target_seqs: ${max_target_seqs}"
     }
 
     function merge_sgp_fasta() {
-        touch "${taskdir}/sgp.fasta"
+        touch ${taskdir}/sgp.fasta
         for org in "${org_sgp[@]}"; do
             genus=${org%%_*}
             tmpfile=$(mktemp)
             python3 -m fasp prefix_to_sequence_ids \
-                "${DATA}/${genus}/${org}.pep.all.fasta" \
-                "$tmpfile" \
-                "sgp_${org}_"
-            cat "$tmpfile" >> "${taskdir}/sgp.fasta"
-            rm "$tmpfile"
+                ${DATA}/${genus}/${org}.pep.all.fasta \
+                $tmpfile \
+                sgp_${org}_
+            cat $tmpfile >> ${taskdir}/sgp.fasta
+            rm $tmpfile
         done
     }
 
     function merge_grp_fasta() {
-        touch "${taskdir}/grp.fasta"
+        touch ${taskdir}/grp.fasta
         for org in "${org_grp[@]}"; do
             genus=${org%%_*}
             tmpfile=$(mktemp)
             python3 -m fasp prefix_to_sequence_ids \
-                "${DATA}/${genus}/${org}.pep.all.fasta" \
-                "$tmpfile" \
-                "grp_${org}_"
-            cat "$tmpfile" >> "${taskdir}/grp.fasta"
-            rm "$tmpfile"
+                ${DATA}/${genus}/${org}.pep.all.fasta \
+                $tmpfile \
+                grp_${org}_
+            cat $tmpfile >> ${taskdir}/grp.fasta
+            rm $tmpfile
         done
     }
 
     function merge_ogp_fasta() {
-        touch "${taskdir}/ogp.fasta"
+        touch ${taskdir}/ogp.fasta
         for org in "${org_ogp[@]}"; do
             genus=${org%%_*}
             tmpfile=$(mktemp)
             python3 -m fasp prefix_to_sequence_ids \
-                "${DATA}/${genus}/${org}.pep.all.fasta" \
-                "$tmpfile" \
-                "ogp_${org}_"
-            cat "$tmpfile" >> "${taskdir}/ogp.fasta"
-            rm "$tmpfile"
+                ${DATA}/${genus}/${org}.pep.all.fasta \
+                $tmpfile \
+                ogp_${org}_
+            cat $tmpfile >> ${taskdir}/ogp.fasta
+            rm $tmpfile
         done
     }
 
-    function makedb_reference() {
-        touch "${taskdir}/reference.fasta"
+    function make_database() {
+        touch ${taskdir}/reference.fasta
         for ref in sgp grp ogp; do
-            cat "${taskdir}/${ref}.fasta" >> "${taskdir}/reference.fasta"
+            cat ${taskdir}/${ref}.fasta >> ${taskdir}/reference.fasta
         done
         diamond makedb \
-            --in "${taskdir}/reference.fasta" \
-            --db "${taskdir}/database"
+            --in ${taskdir}/reference.fasta \
+            --db ${taskdir}/database
     }
 
-    function blastp_with_diamond() {
+    function search_homology() {
         diamond blastp \
-            --db "${taskdir}/database" \
-            --query "${taskdir}/sgp.fasta" \
-            --out "${taskdir}/hits.tsv" \
-            --max-target-seqs "$max_target_seqs"
+            --db ${taskdir}/database \
+            --query ${taskdir}/sgp.fasta \
+            --out ${taskdir}/hits.tsv \
+            --max-target-seqs $max_target_seqs
     }
 
-    function slice_hits() {
+    function detect_crossover() {
         python3 -m biotp slice_hits_by_crossover_group \
-            "${taskdir}/hits.tsv" \
-            "${taskdir}/hits_slice.tsv"
+            ${taskdir}/hits.tsv \
+            ${taskdir}/hits_slice.tsv
         python3 -m biotp output_besthit_for_subgroup \
-            "${taskdir}/hits.tsv" \
-            "${taskdir}/besthits.tsv"
+            ${taskdir}/hits.tsv \
+            ${taskdir}/besthits.tsv
     }
 
-    function merge_sgp_fasta_2() {
-        touch "${taskdir}/sgp.cds.all.fasta"
+    function merge_sgp_fasta_cds() {
+        touch ${taskdir}/sgp.cds.all.fasta
         for org in "${org_sgp[@]}"; do
             genus=${org%%_*}
             tmpfile1=$(mktemp)
             tmpfile2=$(mktemp)
             python3 -m fasp rename_headers_feature \
-                "${DATA}/${genus}/${org}.cds.all.fasta" \
-                "$tmpfile1" \
-                "protein_id"
+                ${DATA}/${genus}/${org}.cds.all.fasta \
+                $tmpfile1 \
+                protein_id
             python3 -m fasp prefix_to_sequence_ids \
-                "$tmpfile1" \
-                "$tmpfile2" \
-                "sgp_${org}_"
-            cat "$tmpfile2" >> "${taskdir}/sgp.cds.all.fasta"
-            rm "$tmpfile1" "$tmpfile2"
+                $tmpfile1 \
+                $tmpfile2 \
+                sgp_${org}_
+            cat $tmpfile2 >> ${taskdir}/sgp.cds.all.fasta
+            rm $tmpfile1 $tmpfile2
         done
     }
 
-    function merge_grp_fasta_2() {
-        touch "${taskdir}/grp.cds.all.fasta"
+    function merge_grp_fasta_cds() {
+        touch ${taskdir}/grp.cds.all.fasta
         for org in "${org_grp[@]}"; do
             genus=${org%%_*}
             tmpfile1=$(mktemp)
             tmpfile2=$(mktemp)
             python3 -m fasp rename_headers_feature \
-                "${DATA}/${genus}/${org}.cds.all.fasta" \
-                "$tmpfile1" \
-                "protein_id"
+                ${DATA}/${genus}/${org}.cds.all.fasta \
+                $tmpfile1 \
+                protein_id
             python3 -m fasp prefix_to_sequence_ids \
-                "$tmpfile1" \
-                "$tmpfile2" \
-                "grp_${org}_"
-            cat "$tmpfile2" >> "${taskdir}/grp.cds.all.fasta"
-            rm "$tmpfile1" "$tmpfile2"
+                $tmpfile1 \
+                $tmpfile2 \
+                grp_${org}_
+            cat $tmpfile2 >> ${taskdir}/grp.cds.all.fasta
+            rm $tmpfile1 $tmpfile2
         done
     }
 
-    function merge_ogp_fasta_2() {
-        touch "${taskdir}/ogp.cds.all.fasta"
+    function merge_ogp_fasta_cds() {
+        touch ${taskdir}/ogp.cds.all.fasta
         for org in "${org_ogp[@]}"; do
             genus=${org%%_*}
             tmpfile1=$(mktemp)
             tmpfile2=$(mktemp)
             python3 -m fasp rename_headers_feature \
-                "${DATA}/${genus}/${org}.cds.all.fasta" \
-                "$tmpfile1" \
-                "protein_id"
+                ${DATA}/${genus}/${org}.cds.all.fasta \
+                $tmpfile1 \
+                protein_id
             python3 -m fasp prefix_to_sequence_ids \
-                "$tmpfile1" \
-                "$tmpfile2" \
-                "ogp_${org}_"
-            cat "$tmpfile2" >> "${taskdir}/ogp.cds.all.fasta"
-            rm "$tmpfile1" "$tmpfile2"
+                $tmpfile1 \
+                $tmpfile2 \
+                ogp_${org}_
+            cat $tmpfile2 >> ${taskdir}/ogp.cds.all.fasta
+            rm $tmpfile1 $tmpfile2
         done
     }
 
-    function makeblastdb_reference() {
-        touch "${taskdir}/reference.cds.all.fasta"
+    function make_database_cds() {
+        touch ${taskdir}/reference.cds.all.fasta
         for ref in sgp grp ogp; do
-            cat "${taskdir}/${ref}.cds.all.fasta" >> "${taskdir}/reference.cds.all.fasta"
+            cat ${taskdir}/${ref}.cds.all.fasta >> ${taskdir}/reference.cds.all.fasta
         done
         tmpfile1=$(mktemp)
         tmpfile2=$(mktemp)
-        cut -f 1 "${taskdir}/hits_slice.tsv" | sort -u | xargs -n 1000 > "$tmpfile1"
-        cut -f 2 "${taskdir}/hits_slice.tsv" | sort -u | xargs -n 1000 > "$tmpfile2"
+        cut -f 1 ${taskdir}/hits_slice.tsv | sort -u | xargs -n 1000 > $tmpfile1
+        cut -f 2 ${taskdir}/hits_slice.tsv | sort -u | xargs -n 1000 > $tmpfile2
         python3 -m fasp slice_records_by_idfile \
-            "${taskdir}/sgp.cds.all.fasta" \
-            "${taskdir}/sgp.cds.fasta" \
-            "$tmpfile1"
+            ${taskdir}/sgp.cds.all.fasta \
+            ${taskdir}/sgp.cds.fasta \
+            $tmpfile1
         python3 -m fasp slice_records_by_idfile \
-            "${taskdir}/reference.cds.all.fasta" \
-            "${taskdir}/reference.cds.fasta" \
-            "$tmpfile2"
-        rm "$tmpfile1" "$tmpfile2"
+            ${taskdir}/reference.cds.all.fasta \
+            ${taskdir}/reference.cds.fasta \
+            $tmpfile2
+        rm $tmpfile1 $tmpfile2
         python3 -m fasp assign_unique_ids \
-            "${taskdir}/reference.cds.fasta" \
-            "${taskdir}/reference.cds.unique.fasta" 
+            ${taskdir}/reference.cds.fasta \
+            ${taskdir}/reference.cds.unique.fasta
         makeblastdb \
-            -in "${taskdir}/reference.cds.unique.fasta" \
-            -out "${taskdir}/database_2" \
+            -in ${taskdir}/reference.cds.unique.fasta \
+            -out ${taskdir}/database_cds \
             -dbtype nucl -hash_index -parse_seqids
     }
 
-    function blastn_hits() {
+    function search_homology_cds() {
         blastn -outfmt 6 -evalue 10 \
-            -db "${taskdir}/database_2" \
-            -query "${taskdir}/sgp.cds.fasta" \
-            -out "${taskdir}/hits_2.tsv" \
+            -db ${taskdir}/database_cds \
+            -query ${taskdir}/sgp.cds.fasta \
+            -out ${taskdir}/hits_cds.tsv \
             -max_target_seqs $max_target_seqs
     }
 
-    function slice_hits_2() {
+    function detect_crossover_cds() {
         python3 -m biotp slice_hits_by_crossover_group \
-            "${taskdir}/hits_2.tsv" \
-            "${taskdir}/hits_slice_2.tsv"
+            ${taskdir}/hits_cds.tsv \
+            ${taskdir}/hits_slice_cds.tsv
         python3 -m biotp output_besthit_for_subgroup \
-            "${taskdir}/hits_2.tsv" \
-            "${taskdir}/besthits_2.tsv"
+            ${taskdir}/hits_cds.tsv \
+            ${taskdir}/besthits_cds.tsv
     }
 
     function main() {
@@ -231,15 +224,15 @@ EOS
         merge_sgp_fasta
         merge_grp_fasta
         merge_ogp_fasta
-        makedb_reference
-        blastp_with_diamond
-        slice_hits
-        merge_sgp_fasta_2
-        merge_grp_fasta_2
-        merge_ogp_fasta_2
-        makeblastdb_reference
-        blastn_hits
-        slice_hits_2
+        make_database
+        search_homology
+        detect_crossover
+        merge_sgp_fasta_cds
+        merge_grp_fasta_cds
+        merge_ogp_fasta_cds
+        make_database_cds
+        search_homology_cds
+        detect_crossover_cds
     }
 
     main "$@"
