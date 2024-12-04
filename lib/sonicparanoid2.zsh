43727098ac19723d3724c28656e972648d260b82
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-12-04 20:20:00 +0900

    update sonicparanoid2.zsh

--- lib/sonicparanoid2.zsh
+++ lib/sonicparanoid2.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
-# Last updated: 2024-10-30
-
-#### Function libs with SonicParanoid2 (Cosentino et al., 2024)
+# Last updated: 2024-12-04
+# Tools: SonicParanoid2
+# Function libs with SonicParanoid2
 : << 'FUNCTIONS'
 identify_ortholog_groups: Identify ortholog groups.
 FUNCTIONS
@@ -11,8 +11,8 @@ function identify_ortholog_groups() {
         cat <<EOS
 Usage: identify_ortholog_groups <arg1> <arg2> <arg3>
 
-    arg1: num       <- Number of organisms.      (e.g., 4)
-    arg2: org       <- Names  of organisms.      (e.g., "Cuscuta australis" "Cuscuta campestris" ... "Cuscuta europaea")
+    arg1: num       <- Number of organisms.      (e.g., 3)
+    arg2: org       <- Names  of organisms.      (e.g., "Cuscuta australis" "Cuscuta campestris" "Cuscuta europaea")
     arg3: threads   <- Number of threads to use. (e.g., 2)
 
 EOS
@@ -32,23 +32,23 @@ EOS
     }
 
     function move_fasta_to_taskdir() {
-        mkdir ${taskdir}/input ${taskdir}/output
+        mkdir "${taskdir}/input" "${taskdir}/output"
         for org in "${orgs[@]}"; do
             genus=${org%%_*}
             tmpfile1=$(mktemp)
             tmpfile2=$(mktemp)
             python3 -m fasp exclude_isoforms_by_length \
-                ${DATA}/${genus}/${org}.pep.all.fasta \
-                $tmpfile1 \
-                ${DATA}/${genus}/${org}.genome.gff 
+                "${DATA}/${genus}/${org}.pep.all.fasta" \
+                "$tmpfile1" \
+                "${DATA}/${genus}/${org}.genome.gff" 
             python3 -m fasp exclude_non_nuclear_proteins \
-                $tmpfile1 \
-                $tmpfile2
+                "$tmpfile1" \
+                "$tmpfile2"
             python3 -m fasp prefix_to_sequence_ids \
-                $tmpfile2 \
-                ${taskdir}/input/${org}.fasta \
-                ${org}
-            rm $tmpfile1 $tmpfile2
+                "$tmpfile2" \
+                "${taskdir}/input/${org}.fasta" \
+                "${org}"
+            rm "$tmpfile1" "$tmpfile2"
         done
     }
 
@@ -56,7 +56,7 @@ EOS
         parse_args "$@"
         make_taskdir
         move_fasta_to_taskdir
-        sonicparanoid -i ${taskdir}/input -o ${taskdir}/output -t $threads
+        sonicparanoid -i "${taskdir}/input" -o "${taskdir}/output" -t $threads
     }
 
     main "$@"
