dea01927db113e602339abf1ad3f39b248f5d0c2
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-02-10 13:49:29 +0900

    feat: a2a_diamond

--- lib/bithon.zsh
+++ lib/bithon.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 # Last updated: 2025-2-7
-# Tools: bithon BLAST+
+# Tools: bithon BLAST+ diamond
 # Function libs with bithon
 : << 'FUNCTIONS'
 build_longest_isoform: Build the longest isoform from NCBI/ENSEMBL genome data using bithon.
 a2a_blastp: All to All Protein-Protein BLAST.
+a2a_diamond: All to All Protein-Protein BLAST using diamond.
 FUNCTIONS
 
 function build_longest_isoform() {
@@ -155,6 +156,89 @@ EOS
             -db "${taskdir}/input.fasta" \
             -query "${taskdir}/input.fasta" \
             -out "${taskdir}/hits.txt"
+    }
+
+    main "$@"
+}
+
+function a2a_diamond() {
+    function usage() {
+        cat << EOS
+Usage: a2a_diamond <arg1> <arg2> <arg3>
+
+    arg1: sp_name_1 <- Name of species. (e.g., "Cuscuta australis")
+    arg2: sp_name_2 <- Name of species. (e.g., "Cuscuta campestris")
+    arg3: max_ts    <- Max target sequnces. (e.g., 10)
+
+EOS
+        exit 1
+    }
+
+    function parse_args() {
+        if [[ $# -lt 2 ]]; then
+            usage
+        fi
+        sp_names=()
+        for ((i=1; i<=2; i++)); do
+            sp_names[i]="${1// /_}"; shift
+            echo "${i}:${sp_names[i]}"
+        done
+        ## default evalue
+        max_ts=10
+
+        if [[ $# != 0 ]]; then
+            max_ts=$1; shift
+        fi
+    }
+
+    function build_fasta() {
+        mkdir "${taskdir}/input"
+        for sp_name in "${sp_names[@]}"; do
+            ## ENSEMBL
+            if [[ -e "${DATA}/Ensembl/${sp_name}.pep.all.fasta" ]]; then
+                bithon ensgls -i "${DATA}/ENSEMBL/${sp_name}.pep.all.fasta" -o "${taskdir}/input/${sp_name}.fasta" --header transcript
+                continue
+            fi
+
+            ## NCBI
+            mkdir "${taskdir}/input/${sp_name}"
+            gn_name=${sp_name%%_*}
+            cp "${DATA}/${gn_name}/${sp_name}.pep.all.fasta" "${taskdir}/input/${sp_name}/pep.fasta"
+            cp "${DATA}/${gn_name}/${sp_name}.cds.all.fasta" "${taskdir}/input/${sp_name}/cds.fasta"
+            bithon gls -i "${taskdir}/input/${sp_name}" -o "${taskdir}/input/${sp_name}"
+            cp "${taskdir}/input/${sp_name}/longest.pep.fasta" "${taskdir}/input/${sp_name}.fasta"
+            rm -r "${taskdir}/input/${sp_name}"
+        done
+    }
+
+    function setup_fasta() {
+        touch "${taskdir}/input.fasta"
+        for sp_name in "${sp_names[@]}"; do
+            sp_part=(${(s:_:)sp_name})
+            sp="${sp_part[1]:0:1}${sp_part[2]}"
+            tmpfile=$(mktemp)
+            python3 -m fasp prefix_to_sequence_ids \
+                "${taskdir}/input/${sp_name}.fasta" \
+                "$tmpfile" \
+                "$sp"
+            cat "$tmpfile" >> "${taskdir}/input.fasta"
+        done
+
+        diamond makedb \
+            --in ${taskdir}/input.fasta \
+            --db ${taskdir}/database
+    }
+
+    function main() {
+        parse_args "$@"
+        make_taskdir
+        build_fasta
+        setup_fasta
+        diamond blastp \
+            --db ${taskdir}/database \
+            --query ${taskdir}/database.fasta \
+            --out ${taskdir}/hits.txt \
+            --max-target-seqs $max_ts
     }
 
     main "$@"
